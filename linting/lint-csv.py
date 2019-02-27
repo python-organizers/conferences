3a4953f307048192b5d14edb4a6f6e77521bae85
David Fischer <djfische@gmail.com>
2019-02-27 10:56:23 -0800

    Accept linting suggestions

--- linting/lint-csv.py
+++ linting/lint-csv.py
@@ -7,25 +7,28 @@ CSV_PATH = os.path.join(os.path.dirname(__file__), '..')
 
 # This is the default csvclean message
 # when there are no errors
-NO_ERRORS_MSG = b'No errors.\n'
+NO_ERRORS_MSG = 'No errors.\n'
 
 
 def main():
     is_error = False
     for filename in os.listdir(CSV_PATH):
-        if filename.endswith('.csv'):
-            filepath = os.path.join(CSV_PATH, filename)
+        if not filename.endswith('.csv'):
+            continue
 
-            # In the future, csvclean will probably set the status code
-            # when there are CSV validation errors
-            # See: https://github.com/wireservice/csvkit/pull/781
-            output_bytes = subprocess.check_output(
-                ['csvclean', '-n', filepath],
-                stderr=subprocess.STDOUT,
-            )
-            if output_bytes != NO_ERRORS_MSG:
-                print(output_bytes.decode('utf-8'))
-                is_error = True
+        filepath = os.path.join(CSV_PATH, filename)
+
+        # In the future, csvclean will probably set the status code
+        # when there are CSV validation errors
+        # See: https://github.com/wireservice/csvkit/pull/781
+        output = subprocess.check_output(
+            ['csvclean', '-n', filepath],
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+        if output != NO_ERRORS_MSG:
+            print(output)
+            is_error = True
 
     if is_error:
         sys.exit(1)
