26ed17c67f2c8fbd3da74a682193fa585ccf24a7
Roy Hyunjin Han <rhh@crosscompute.com>
2024-05-21 12:53:54 -0400

    Remove blank rows

--- linters/autoformat-csv.py
+++ linters/autoformat-csv.py
@@ -8,22 +8,22 @@ FOLDER = Path(__file__).resolve().parent.parent
 def main():
     for path in FOLDER.glob('*.csv'):
         with path.open(mode='rt') as f:
-            reader = DictReader(f, dialect='unix')
-            rows = list(reader)
-
+            rows = list(DictReader(f, dialect='unix'))
         rows.sort(key=lambda row: (
             row['Start Date'], row['End Date'], row['Subject']))
-
+        new_rows = []
         for row in rows:
             for key, value in row.items():
                 row[key] = value.strip()
-
+            if not row['Subject']:
+                continue
+            new_rows.append(row)
         with path.open(mode='wt') as f:
             writer = DictWriter(
-                f, fieldnames=rows[0].keys(), dialect='unix',
+                f, fieldnames=new_rows[0].keys(), dialect='unix',
                 quoting=QUOTE_MINIMAL)
             writer.writeheader()
-            writer.writerows(rows)
+            writer.writerows(new_rows)
 
 
 if __name__ == '__main__':
